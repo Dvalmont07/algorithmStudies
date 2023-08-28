f9e21841567abd2e95fe5b886cab3b06badf083f
Danilo Matos <danilo.dejesusmatos@gmail.com>
2023-08-27 21:31:36 -0300

    fix logic

--- graph/breadthFirstSearch/main.js
+++ graph/breadthFirstSearch/main.js
@@ -1,49 +1,44 @@
 function breadthFirstSearch(peopleNodeListDictionary, searchedItem) {
-    /**Searching the first salesperson in the peopleQueue */
-    let peopleQueue = [];
-    peopleQueue = peopleQueue.concat(peopleNodeListDictionary['me']);
+    if (Object.values(peopleNodeListDictionary).length === 0) return emptyDictionayMessage();
+    /**Searching the first salesperson in the peopleQueued */
+    let peopleQueued = [].concat(peopleNodeListDictionary['me']);
     const verifiedPeople = [];
 
-    if (peopleNodeListDictionary) {
-        while (peopleQueue.length > 0) {
-            const person = getFirstPersonFromQueue();
-            if (isNotInVerifiedPeople(person)) {
-                if (isSeller(person)) {
-                    return `Result: <strong>${person}</strong> is a seller`;
-                } else {
-                    verifiedPeople.push(person);
-                    peopleQueue = peopleQueue.concat(peopleNodeListDictionary[person]);
-                }
+    while (peopleQueued.length > 0) {
+        const person = getFirstPersonFromQueue();
+        if (isNotInVerifiedPeople(person)) {
+            if (isSeller(person)) {
+                return `Result: <strong>${person}</strong> is a seller`;
+            } else {
+                verifiedPeople.push(person);
+                peopleQueued = peopleQueued.concat(peopleNodeListDictionary[person]);
             }
         }
-        return emptyDictionayMessage();
-    } else {
-        return emptyDictionayMessage();
     }
+    return noSalesPersonMessage();
 
     function isNotInVerifiedPeople(person) {
         return verifiedPeople.indexOf(person) == -1;
     }
-
     function getFirstPersonFromQueue() {
-        return peopleQueue.shift();
+        return peopleQueued.shift();
     }
-
     function emptyDictionayMessage() {
+        return `The disctionaryis empty`;
+    }
+    function noSalesPersonMessage() {
         return `There's no salesperson in the list`;
     }
-
     function isSeller(person) {
         return person.endsWith(searchedItem);
     }
-
 }
 function showGraphBreadthFirstSearch() {
     const searchedItem = '-s';
     document.write(`NodeList: <br> ${getNodesListToString()}`);
     document.write(`<h4>Expected result: <strong>jonny-s</strong> is a seller</h4>`);
     document.write(`<h4>Searched Item: '-s'<h4>`);
-    
+
     document.write(`${breadthFirstSearch(getNodesList(), searchedItem)}`);
 
     function getNodesList() {
@@ -55,7 +50,7 @@ function showGraphBreadthFirstSearch() {
         nodesList['anuj'] = [];
         nodesList['peggy'] = [];
         nodesList['thom'] = [];
-        nodesList['jonny-s'] = [];
+        nodesList['jonny'] = [];
         return nodesList;
     }
 
--- sorting/quickSort/main.js
+++ sorting/quickSort/main.js
@@ -2,20 +2,19 @@ function quickSort(aArray) {
 
     if (aArray.length < 2) {
         return aArray;
-    } else {
-        const random = Math.floor(Math.random() * (aArray.length - 1));
-        let pivot = aArray[random];
-        let loewrThanPivot = [];
-        let higherThanPivot = [];
-        for (let i = 0; i < aArray.length; i++) {
-            if (aArray[i] < pivot) {
-                loewrThanPivot.push(aArray[i]);
-            } else if (aArray[i] > pivot) {
-                higherThanPivot.push(aArray[i]);
-            }
-        }
-        return quickSort(loewrThanPivot).concat(pivot).concat(quickSort(higherThanPivot));
     }
+    const random = Math.floor(Math.random() * (aArray.length - 1));
+    let pivot = aArray[random];
+    let loewrThanPivot = [];
+    let higherThanPivot = [];
+    for (let i = 0; i < aArray.length; i++) {
+        if (aArray[i] < pivot) {
+            loewrThanPivot.push(aArray[i]);
+        } else if (aArray[i] > pivot) {
+            higherThanPivot.push(aArray[i]);
+        }
+    }
+    return quickSort(loewrThanPivot).concat(pivot).concat(quickSort(higherThanPivot));
 }
 
 function showQuickSorting() {
